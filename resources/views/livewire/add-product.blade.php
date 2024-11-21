373cb9fe9bc5be532d6d46d8a5bcd006a02d3817
donguyentoan2003 <donguyentoan2003@gmail.com>
2024-11-21 09:45:50 +0700

    update_v4

--- resources/views/livewire/add-product.blade.php
+++ resources/views/livewire/add-product.blade.php
@@ -195,23 +195,25 @@
                                                             <div class="flex bg-slate-300 items-center rounded-lg">
                                                                 <div>
                                                                     <li class='w-fit  px-2 py-1 h-fit  text-black'>
-                                                                        {{$variant['nameOption']}}
+                                                                      <p> {{$variant['nameOption']}}
+                                                                        <div wire:click="removeVariantOption('{{$variant['nameOption']}}')">
+                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-x font-bold" viewBox="0 0 16 16">
+                                                                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                                                                              </svg>
+                                                                        </div>    
+                                                                    </p> 
                                                                       
                                                                     </li>
                                                                 </div>
                                                                
-                                                                <div wire:click="removeVariantOption('{{$variant['nameOption']}}')">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-x font-bold" viewBox="0 0 16 16">
-                                                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                                                                      </svg>
-                                                                </div>
+                                                               
                                                             </div>
                                                                
                                                             @endforeach
                                                         </ul>
                                                         <button
                                                             wire:click="editVariantProduct('{{$variantName}}')"
-                                                            class='text-black underline mt-2'
+                                                            class='text-red-400  '
                                                         >
                                                             {{ $isEditing && $editingVariantName === $variantName ? 'Editing...' : 'Edit' }}
                                                         </button>
